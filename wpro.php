451eb27cb563c5e8e602c3303dfbba9310d0591e
Reimund Trost <reimund@code7.se>
2014-11-13 16:05:43 +0100

    Don't use https protocol when virtual host is enabled.

--- wpro.php
+++ wpro.php
@@ -509,7 +509,7 @@ class WordpressReadOnly extends WordpressReadOnlyGeneric {
 			break;
 		default:
 			if (wpro_get_option('wpro-aws-virthost')) {
-				$data['baseurl'] = 'https://' . trim(str_replace('//', '/', wpro_get_option('wpro-aws-bucket') . '/' . trim(wpro_get_option('wpro-folder'))), '/');
+				$data['baseurl'] = 'http://' . trim(str_replace('//', '/', wpro_get_option('wpro-aws-bucket') . '/' . trim(wpro_get_option('wpro-folder'))), '/');
 			} else {
 				$data['baseurl'] = 'https://' . trim(str_replace('//', '/', wpro_get_option('wpro-aws-bucket') . '.s3.amazonaws.com/' . trim(wpro_get_option('wpro-folder'))), '/');
 			}
